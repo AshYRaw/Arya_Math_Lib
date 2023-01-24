cd51181f4d6a83430aa3f5bacf46d86ddb35aa50
AshishYRawat <60941854+AshishYRawat@users.noreply.github.com>
2023-01-24 00:11:32 -0800

    Add files via upload

---
+++ examples/Main.cpp
@@ -0,0 +1,16 @@
+#include <Arya/VectorX.h>
+#include <Arya/MatrixX.h>
+#include<stdio.h>
+int main()
+{
+	hello();
+	//VectorXf* testvector = createVector(true, 3);
+	VectorXf* TestVector = createVector(true, 3);
+	printf("\n\n");
+	PrintVect(TestVector);
+	printf("\n\n");
+
+	MatrixXf* TestMatrix = createMatrix(3,3);
+	PrintMat(TestMatrix);
+	return 0;
+}
